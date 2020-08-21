3ff315380fb2e6a5fcf8c144566afe2db10b7e76
Arseniy Chekanov <senya@chekanov.net>
2020-08-21 14:48:48 +0300

    Fix free RAM hs program for i3blocks

--- modules/workspace/i3blocks/scripts/free.hs
+++ modules/workspace/i3blocks/scripts/free.hs
@@ -2,9 +2,10 @@ import System.Exit
 
 main :: IO ()
 main = do
+  -- Read available memory in kB
   freeMemory <- read
                 <$> (!!1) <$> words
                 <$> (!!2) <$> lines
                 <$> readFile "/proc/meminfo"
-  putStrLn $ (take 3 $ show $ freeMemory / (2 ^ 30)) ++ "Gb"
-  exitWith $ if freeMemory > (2 ^ 30) then ExitSuccess else ExitFailure 33
+  putStrLn $ (take 5 $ show $ (freeMemory / (2 ^ 20))) ++ "Gb"
+  exitWith $ if freeMemory > (2 ^ 20) then ExitSuccess else ExitFailure 33
